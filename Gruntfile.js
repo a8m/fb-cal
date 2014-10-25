cdde02c2da478b9fc0339194cc223e5a53cd146a
Ariel Mashraki <ariel@mashraki.co.il>
2014-10-25 14:43:46 +0300

    fix(npm,grunt): remove cdnify

--- Gruntfile.js
+++ Gruntfile.js
@@ -247,13 +247,6 @@ module.exports = function (grunt) {
       }
     },
 
-    // Replace Google CDN references
-    cdnify: {
-      dist: {
-        html: ['<%= yeoman.dist %>/*.html']
-      }
-    },
-
     // Copies remaining files to places other tasks can use
     copy: {
       dist: {
@@ -345,7 +338,6 @@ module.exports = function (grunt) {
     'concat',
     'ngAnnotate',
     'copy:dist',
-    'cdnify',
     'cssmin',
     'uglify',
     'filerev',
